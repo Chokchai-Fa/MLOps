e5d1f2ac79c4e28a37c18bf5b4effafd89227378
chokchai <phukao1234@gmail.com>
2023-07-15 19:35:25 +0700

    init jenkins and pipeline

---
+++ Job01-MLOps.groovy
@@ -0,0 +1,20 @@
+pipeline {
+    agent any
+
+    stages{
+        stage('Build')
+            steps{
+                git 'https://github.com/Chokchai-Fa/banking-go'
+
+            }
+    
+    post {
+        always{
+            sh 'ls ./src'
+        }
+    }
+    }
+
+
+
+}
