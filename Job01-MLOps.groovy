0c5e55bc2ed007c618a22d002c47352c3bb7e511
chokchai <phukao1234@gmail.com>
2023-07-27 16:03:49 +0700

    add script

--- Job01-MLOps.groovy
+++ Job01-MLOps.groovy
@@ -5,15 +5,15 @@ pipeline {
         stage('Pull Code') {
             steps{
                 dir('src'){
-                     sh 'git clone https://github.com/Chokchai-Fa/go-hello'
+                     sh 'https://github.com/Chokchai-Fa/MLOps-Project01o'
                 }
             }
         }
         stage('Build Image and Push Image to Regisrty'){
             steps{
-                   dir('src/go-hello'){
+                   dir('src/MLOps-Project01'){
                     script{
-                        def dockerImage = docker.build('chokchaifa/go-hello', '.')
+                        def dockerImage = docker.build('chokchaifa/mlops-test', '.')
                         docker.withRegistry('https://registry.hub.docker.com', 'docker-hub-credentials') {
                             dockerImage.push()
                         }
@@ -22,32 +22,57 @@ pipeline {
             }
         }
          
-        stage('Pull Image'){
+        stage('Pull Image Stage'){
             steps{
                 script{
-                    docker.image('chokchaifa/go-hello:latest').pull()
+                    docker.image('chokchaifa/mlops-test').pull()
                 }
             }
         }
 
-        stage('Deploy'){
+        stage('Deploy Stage'){
             steps{
                 script{
-                    def containerName = 'hello-go'
+                    def containerName = 'ml-ops01'
                     def existingContainerId = sh(returnStdout: true, script: "docker ps -aqf \"name=${containerName}\"").trim()
                     if (!existingContainerId.isEmpty()) {
                         sh "docker stop ${existingContainerId}"
                         sh "docker rm ${existingContainerId}"
                     }
-                    docker.image('chokchaifa/go-hello:latest').run('--name hello-go -d -p 3000:8080')
+                    docker.image('chokchaifa/mlops-test:latest').run('-dit --name mlops-test -d --gplus all ')
                 }
             }
                 
             }
+
+
+        stage('PreProcessing Stage'){
+            steps{
+                script{
+                  sh 'docker exec -it mlops-test  python3 preprocessing.py'
+                }
+            } 
+            }
+
         }
 
-    
-
+        stage('Train Stage'){
+            steps{
+                script{
+                  sh 'docker exec -it mlops-test  python3 train.py'
+                }
+            } 
+            }
+            
+        stage('Test Stage'){
+        steps{
+            script{
+                sh 'docker exec -it mlops-test  python3 test.py'
+                sh 'docker exec -it mlops-test  cat /results/train_metadata.json /results/test_metadata.json'
+            }
+        } 
+        }
+        
       post {
         always{
           cleanWs()
